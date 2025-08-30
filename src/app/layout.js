2137c730d9ab76ceb4fc7f33b4d4e9557f114b9b
lucian-xinitrc <cusmirlucian@gmail.com>
2025-08-30 13:25:26 +0000

    Meta Changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,37 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Lucian Web Services",
   description: "Created by Mebhevy Services",
+  keywords: ['Next.js', 'SEO', 'Thumbnail', 'Tutorial'],
+  authors: [{ name: 'Lucian-Florin' }],
+  viewport: 'width=device-width, initial-scale=1.0',
+
+  // Open Graph (Facebook, WhatsApp, LinkedIn)
+  openGraph: {
+    title: "Lucian Web Services",
+    description: "Created by Mebhevy Services",
+    url: 'https://lucianws.com',
+    siteName: 'LucianWS',
+    images: [
+      {
+        url: '/images/thumbnail.png', // imaginea ta
+        width: 1200,
+        height: 630,
+        alt: 'Mebhevy',
+      },
+    ],
+    locale: 'en_EN',
+    type: 'website',
+  },
+
+  // Twitter Cards
+  twitter: {
+    card: '/images/thumbnail.png',
+    title: 'Lucian Web Services',
+    description: 'Lets built the web together',
+    images: ['/images/thumbnail.png'],
+    site: 'https://lucianws.com',
+    creator: 'Lucian-Florin',
+  },
 };
 
 export default function RootLayout({ children }) {
